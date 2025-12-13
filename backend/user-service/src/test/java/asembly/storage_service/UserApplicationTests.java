67772bfcb32938997b7f3613394db4b3161517fd
Asembly <darklayergd@gmail.com>
2025-12-13 19:34:56 +0300

    feat: Add docker file

--- backend/user-service/src/test/java/asembly/storage_service/UserApplicationTests.java
+++ backend/user-service/src/test/java/asembly/storage_service/UserApplicationTests.java
@@ -1,9 +1,10 @@
 package asembly.storage_service;
 
+import asembly.user_service.UserApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = UserApplication.class)
 class UserApplicationTests {
 
 	@Test
